43e4ac976f3f860dbe274f2592277aeff83cbe8b
hi-avani <avanik159@gmail.com>
2026-03-13 11:25:43 +0530

    Improve UI

--- src/masters/DesignationMaster.jsx
+++ src/masters/DesignationMaster.jsx
@@ -183,7 +183,7 @@ export default function DesignationMaster() {
       {!isMobile && (
         <Paper
           elevation={0}
-          sx={{ border: "1px solid #e0e9f5", borderRadius: 3, overflow: "hidden" }}
+          sx={{ border: "1px solid #e0e9f5", borderRadius: 1, overflow: "hidden" }}
         >
           <TableContainer>
             <Table stickyHeader>
